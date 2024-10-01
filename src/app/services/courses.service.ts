b22c8b678127f7c9e6318b556a2664074c2f0ce7
AleksandarDuev <89582096+AleksandarDuev@users.noreply.github.com>
2024-10-01 17:56:01 +0300

    Update courses.service.ts

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-
 import { Course, Author } from "../models";
 
 @Injectable({
@@ -13,11 +12,11 @@ export class CoursesService {
     constructor(private http: HttpClient) {}
 
     getAll(): Observable<Course[]> {
-        return this.http.get<Course[]>(this.apiUrl);
+        return this.http.get<Course[]>(this.apiUrl/courses/all);
     }
 
     createCourse(course: Course): Observable<Course> {
-        return this.http.post<Course>(this.apiUrl, course);
+        return this.http.post<Course>(this.apiUrl/courses/add, course);
     }
 
     editCourse(id: string, course: Course): Observable<Course> {
@@ -25,23 +24,23 @@ export class CoursesService {
     }
 
     getCourse(id: string): Observable<Course> {
-        return this.http.get<Course>(`${this.apiUrl}/${id}`);
+        return this.http.get<Course>(`${this.apiUrl}/courses/${id}`);
     }
 
     deleteCourse(id: string): Observable<void> {
-        return this.http.delete<void>(`${this.apiUrl}/${id}`); // Change to `void` since delete typically returns nothing
+        return this.http.delete<void>(`${this.apiUrl}/courses/${id}`); // Change to `void` since delete typically returns nothing
     }
 
     filterCourses(value: string): Observable<Course[]> {
-        return this.http.get<Course[]>(`${this.apiUrl}?search=${value}`);
+        return this.http.get<Course[]>(`${this.apiUrl}/courses/filter?search=${value}`);
     }
 
     getAllAuthors(): Observable<Author[]> {
-        return this.http.get<Author[]>(`${this.apiUrl}/authors`);
+        return this.http.get<Author[]>(`${this.apiUrl}/authors/all`);
     }
 
     createAuthor(name: string): Observable<Author> {
-        return this.http.post<Author>(`${this.apiUrl}/authors`, {
+        return this.http.post<Author>(`${this.apiUrl}/authors/add`, {
             name,
         });
     }
